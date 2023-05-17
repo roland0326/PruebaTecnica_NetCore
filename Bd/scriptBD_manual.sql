7c967699ffc715b9b77f83f09c6f3f479596618c
roland0326 <rdsolutionsinformatic@gmail.com>
2023-05-16 22:33:54 -0500

    Edit scheme Data base

--- Bd/scriptBD_manual.sql
+++ Bd/scriptBD_manual.sql
@@ -1,7 +1,20 @@
+use master
+GO
 
---drop database [parqueadero]
---create database [parqueadero]
---use parqueadero
+--Validamos si existe la base de datos
+IF EXISTS(select * from sys.databases where name = 'parqueadero')
+BEGIN 
+    DROP DATABASE parqueadero
+END 
+GO
+--creacion de db
+CREATE DATABASE [parqueadero]
+GO 
+USE [parqueadero]
+GO
+
+CREATE SCHEMA [parqueadero];
+GO
 
 create table tipo_identificacion(
 	tipide_id int identity not null,
@@ -11,9 +24,7 @@ create table tipo_identificacion(
 	constraint Unique_tipide_codigo
 	unique (tipide_codigo asc)
 );
-insert into tipo_identificacion(tipide_codigo,tipide_descrip)
-values('13','Cedula de ciudadania'),
-('31','Nit');
+GO
 
 create table ciudad(
 	ciu_id int identity not null,
@@ -23,8 +34,7 @@ create table ciudad(
 	constraint Unique_ciu_codigo
 	unique (ciu_codigo)
 );
-insert into ciudad(ciu_codigo,ciu_nombre)
-values('17001','Manizales');
+GO
 
 create table tipo_vehiculo(
 	tipveh_id int identity not null,
@@ -35,10 +45,7 @@ create table tipo_vehiculo(
 	constraint Unique_tipveh_codigo
 	unique (tipveh_codigo)
 );
-insert into tipo_vehiculo(tipveh_codigo,tipveh_descrip,tipveh_tarifa)
-values('carro','vehiculo de 4 llantas',110),
-('motocicleta','vehiculo de 2 llantas',50),
-('bicicleta','vehiculo no motor',10);
+GO
 
 create table marca(
 	mar_id int identity not null,
@@ -48,8 +55,7 @@ create table marca(
 	constraint Unique_mar_codigo
 	unique (mar_codigo)
 );
-insert into marca(mar_codigo)
-values('hyndai'),('renault'),('chevrolet'),('generico');
+GO
 
 create table sucursal(
 	suc_id int identity not null,
@@ -68,6 +74,7 @@ create table sucursal(
 	constraint fk_ciudad_suc
 	foreign key (ciu_id) references ciudad(ciu_id)
 );
+GO
 
 create table distribucion(
 	dis_id int identity not null,
@@ -81,6 +88,7 @@ create table distribucion(
 	constraint Fk_sucursal_dis
 	foreign key (suc_id) references sucursal(suc_id)
 );
+GO
 
 create table vehiculo(
 	veh_id int identity not null,
@@ -94,6 +102,7 @@ create table vehiculo(
 	constraint Unique_veh_placa
 	unique(veh_placa)
 );
+GO
 
 create table mov_paqueadero(
 	parque_id int identity not null,
@@ -108,8 +117,141 @@ create table mov_paqueadero(
 	constraint Fk_vehiculo_parque
 	foreign key(veh_id) references vehiculo(veh_id),
 	constraint Fk_distribucion_parque
-	foreign key (dis_id) references distribucion(dis_id),
-	constraint Unique_par_fac_dcto
-	unique (par_fac_dcto)
+	foreign key (dis_id) references distribucion(dis_id)
 );
+GO
+
+--datos de inicializacion
+insert into tipo_identificacion(tipide_codigo,tipide_descrip)
+values('13','Cedula de ciudadania'),
+('31','Nit');
+GO
+
+insert into ciudad(ciu_codigo,ciu_nombre)
+values('17001','Manizales');
+GO
+
+insert into tipo_vehiculo(tipveh_codigo,tipveh_descrip,tipveh_tarifa)
+values('carro','vehiculo de 4 llantas',110),
+('motocicleta','vehiculo de 2 llantas',50),
+('bicicleta','vehiculo no motor',10);
+GO
+
+insert into marca(mar_codigo)
+values('hyndai'),('renault'),('chevrolet'),('generico');
+GO
+
+/****** Object:  StoredProcedure [dbo].[Sp_liquidacion]    Script Date: 16/05/2023 5:28:38 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Roland Duque>
+-- Create date: <16/05/2023>
+-- Description:	<Procedimiento para ingreso y salida de vehiculos por sucursal>
+-- =============================================
+CREATE PROCEDURE [dbo].[Sp_liquidacion] 
+	 @placa varchar(20),
+	 @tipo_veh int,
+	 @marca int,
+	 @sucursal int,
+	 @factura varchar(150)
+AS
+BEGIN
+	declare @ValPLaca varchar(20);
+declare @Idveh int;
+declare @PuestoLibre int;
+declare @idparque int;
+declare @inout int;
+declare @parqueado int;
+declare @costo decimal(18,2);
+declare @dcto decimal(18,2);
+declare @manejadcto bit;
+declare @idsalida int;
+declare @horaingreso datetime;
+
+set @inout=0; 
+select @ValPLaca=veh_placa from vehiculo where veh_placa=@placa
+print @ValPLaca
+if(@ValPLaca is null)
+	begin 
+		insert into vehiculo(tipveh_id,mar_id,veh_placa,veh_fec_registro)
+		values(@tipo_veh,@marca,@placa,getdate());
+		set @Idveh= @@IDENTITY
+		set @inout=1
+	end
+else
+	begin 
+		select @Idveh= veh_id from vehiculo where veh_placa=@placa;
+		set @inout=1
+	end
+print @Idveh 
+	--validamos que no este parqueado
+select @parqueado= count(*) from mov_paqueadero where veh_id=@Idveh and parque_hora_out is null
+--print @parqueado
+if(@parqueado>0)
+begin 
+	set @inout=0
+end
+--print @inout
+if (@inout=1)
+	begin
+	select @idparque=min(dis_id)
+	from distribucion as a
+	where dis_habilitado=1 and dis_id not in (select dis_id from mov_paqueadero where parque_hora_out is null)
+	and suc_id=@sucursal;
+
+	insert into mov_paqueadero(veh_id,dis_id,parque_hora_in)
+	values(@Idveh,@idparque,getdate());
+	set @idsalida=@@IDENTITY
+end
+else
+	begin
+		--averiguamos los costos del vehiculo
+		select @idsalida= parque_id , @horaingreso= parque_hora_in from mov_paqueadero where veh_id=@Idveh and parque_hora_out is null
+--print @idsalida
+--print @horaingreso
+		select @manejadcto=suc_maneja_dcto,@dcto=suc_dcto from sucursal where suc_id=@sucursal;
+--print @manejadcto
+--print @dcto
+		select @costo= tipveh_tarifa from tipo_vehiculo where tipveh_id=(select tipveh_id from vehiculo where veh_id=@Idveh)
+--print @costo
+		if(@manejadcto=1 and @factura<>'')
+		begin 
+			set @costo=@costo-(@costo*(@dcto/100))
+		end
+		if(@factura='')
+		begin 
+			set @factura='N/A'+convert(varchar,@idsalida)
+		end
+
+		--select @costo,@factura,@idparque
+		update mov_paqueadero
+		set parque_hora_out=getdate(),
+		parque_costo=@costo,
+		parque_tiempo_min=convert(decimal(18,2),DATEDIFF(MINUTE,@horaingreso,getdate())),
+		par_fac_dcto=@factura
+		where parque_id=@idsalida;
+		
+	end
+	select a.parque_id ParqueId,
+		a.parque_hora_in HoraEntrada,
+		convert(datetime,isnull(a.parque_hora_out,'1900-01-01')) HoraSalida,
+		a.parque_tiempo_min TiempoParqueo,
+		isnull(a.par_fac_dcto,'N/A') DctoDescuento,
+		a.parque_costo ValorPagado,
+		b.veh_placa Placa,
+		c.tipveh_codigo TipoVehiculo,
+		c.tipveh_tarifa Tarifa,
+		d.dis_id IdParqueo,
+		d.dis_sigla+'-'+CONVERT(varchar,d.dis_serie) NumeroParqueo
+		from mov_paqueadero as a
+		inner join vehiculo as b on b.veh_id=a.veh_id
+		inner join tipo_vehiculo as c on c.tipveh_id=b.tipveh_id
+		inner join distribucion as d on d.dis_id=a.dis_id
+		where a.parque_id=@idsalida
+		return
+END
+GO
 
